e9e188e16bbf1ba2aee7f44a93fd97e2f2716801
Michał Mroczka <michal.mroczka03@gmail.com>
2024-06-06 10:17:07 +0200

    Improved footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,78 +1,73 @@
-import { 
-  Container, 
-  Typography,
-  Toolbar, 
-  Box,
-  IconButton, 
-  Button,
-  Divider 
-} from '@mui/material';
+import { Container, Typography, Toolbar, Box, IconButton, Button, Divider } from '@mui/material';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import XIcon from '@mui/icons-material/X';
 import GoogleIcon from '@mui/icons-material/Google';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
-      <footer style={{
-        backgroundColor: "black",
-        color: "white",
-        textAlign: "center",
-        paddingBottom: 60,
-        paddingTop: 48,
-        position: "relative",
-        bottom: "0",
-        left: "0",
-        width: "100%",
-      }}> 
-        <Container>
-          <Toolbar sx={{ flexDirection: "column"}}>
-            <Box sx={{ 
-              justifyContent: "center",
-              marginTop: 0.5
-              }}>
+    <footer style={{
+      backgroundColor: "black",
+      color: "white",
+      textAlign: "center",
+      paddingBottom: 60,
+      paddingTop: 48,
+      position: "relative",
+      bottom: "0",
+      left: "0",
+      width: "100%",
+    }}>
+      <Container>
+        <Toolbar sx={{ flexDirection: "column" }}>
+          <Box sx={{
+            display: 'flex',
+            justifyContent: "center",
+            marginTop: 0.5
+          }}>
             <IconButton aria-label='Facebook' color='inherit'>
-              <FacebookIcon/>
+              <FacebookIcon />
             </IconButton>
             <IconButton aria-label='Instagram' color='inherit'>
-              <InstagramIcon/>
+              <InstagramIcon />
             </IconButton>
             <IconButton aria-label='X' color='inherit'>
-              <XIcon/>
+              <XIcon />
             </IconButton>
             <IconButton aria-label='Google' color='inherit'>
-              <GoogleIcon/>
+              <GoogleIcon />
             </IconButton>
-            </Box>
-            <Box sx={{ 
-              justifyContent: "center",
-              '& Button': {
-                transition: 'text-decoration 0.3s ease-in-out', // Ustawiamy czas trwania na 0.3s
-              },
-              'Button:hover': {
-                textDecoration: "underline"
-              },
-              marginBottom: 1
-              }}>
-              <Button color='inherit'>Home</Button>
-              <Button color='inherit'>About</Button>
-              <Button color='inherit'>Services</Button>
-              <Button color='inherit'>Team</Button>
-              <Button color='inherit'>Contact</Button>
-            </Box>
-            <Divider color="white" sx={{
-              width: {
-                xs: "100%",
-                sm: "90%",
-                md: "80%"
-              },
-              marginBottom: {
-                xs: 1,
-                sm: 1,
-                md: 2,
-              }
-            }}/>
-            <Box>
+          </Box>
+          <Box sx={{
+            display: 'flex',
+            justifyContent: "center",
+            '& Button': {
+              transition: 'text-decoration 0.3s ease-in-out',
+            },
+            'Button:hover': {
+              textDecoration: "underline"
+            },
+            marginBottom: 1
+          }}>
+            <Button component={Link} to="/" color='inherit'>Home</Button>
+            <Button component={Link} to="/about" color='inherit'>About</Button>
+            <Button component={Link} to="/services" color='inherit'>Services</Button>
+            <Button component={Link} to="/team" color='inherit'>Team</Button>
+            <Button component={Link} to="/contact" color='inherit'>Contact</Button>
+          </Box>
+          <Divider color="white" sx={{
+            width: {
+              xs: "100%",
+              sm: "90%",
+              md: "80%"
+            },
+            marginBottom: {
+              xs: 1,
+              sm: 1,
+              md: 2,
+            }
+          }} />
+          <Box>
             <Typography sx={{
               fontSize: 13,
               marginBottom: {
@@ -83,10 +78,10 @@ const Footer = () => {
             }}>
               ©Parking Watcher | All rights reserved
             </Typography>
-            </Box>
-          </Toolbar>
-        </Container>
-      </footer>
+          </Box>
+        </Toolbar>
+      </Container>
+    </footer>
   );
 };
 
